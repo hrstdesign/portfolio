b9ca2fe9d0cb217f0658c543df72bfe2eda26fb5
borisbogdanov <rednose358@gmail.com>
2017-04-17 12:32:52 +0300

    web design gallery and making code more beautiful

--- Logo Design/script.js
+++ Logo Design/script.js
@@ -1,38 +1,44 @@
 $(document).ready(function(){
+	
 	getLogos();
-	$("#logos").magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		gallery: {
-			enabled: true
-		},
-		removalDelay: 300,
-		mainClass: 'animated zoomIn',
-		image: {
-			titleSrc: function(item) {
-				var title = item.el.attr('class')
-				if(title != "null") {
-					return title;
-				} else {
-					return "";
-				};
+	gallery();
+	
+	function gallery() {
+		$("#logos").magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			gallery: {
+				enabled: true
 			},
-			cursor: 'default'
-		}
-	});
-});
-
-function getLogos() {
-	$.ajax({
-		url: "https://api.imgur.com/3/album/yQF1h/images",
-		headers: {"Authorization": "Client-ID e47ec6bd83f70c9"}
-	}).done(function(obj){
-		$.each(obj.data, function(i, item){
-			var full = [];
-			full.push("<a href='" + item.link + "' " + "class='" + item.description + "'>" + "<img src='" + item.link + "'></a>");
-			$(full.join()).appendTo("#logos");
+			removalDelay: 300,
+			mainClass: 'animated zoomIn',
+			image: {
+				titleSrc: function(item) {
+					var title = item.el.attr('class')
+					if(title != "null") {
+						return title;
+					} else {
+						return "";
+					};
+				},
+				cursor: 'default'
+			}
 		});
-	});
-}
+	}
+
+	function getLogos() {
+		$.ajax({
+			url: "https://api.imgur.com/3/album/yQF1h/images",
+			headers: {"Authorization": "Client-ID e47ec6bd83f70c9"}
+		}).done(function(obj){
+			$.each(obj.data, function(i, item){
+				var full = [];
+				full.push("<a href='" + item.link + "' " + "class='" + item.description + "'>" + "<img src='" + item.link + "'></a>");
+				$(full.join()).appendTo("#logos");
+			});
+		});
+	}
+
+});
 
 
